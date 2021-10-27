f4307fcd3b375cb17f97c678463abe61734b6ca7
jim <jbmbroomfield@gmail.com>
2021-10-27 20:11:25 +1100

    Made section headings h1

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 const Section = ({ title, subsections }) => {
     return (
         <div>
-            <p>{title}</p>
+            <h1>{title}</h1>
             {subsections.map(subsection => <p>{subsection.attributes.title}</p>)}
         </div>
     )
