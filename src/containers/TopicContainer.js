0c59ca07a1c5210e3da419533606a261bada1e5a
jim <jbmbroomfield@gmail.com>
2021-10-28 14:35:51 +1100

    Posts appear on thread page

--- src/containers/TopicContainer.js
+++ src/containers/TopicContainer.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
-const TopicContainer = ({ match, subsections, topics }) => {
+import { fetchPosts } from '../actions/postsActions'
+
+const TopicContainer = ({
+    match,
+    subsections, topics, posts,
+    fetchPosts,
+}) => {
     const topicId = match.params.topicId
     const topic = topics.find(topic => parseInt(topic.id) === parseInt(topicId))
+
+    useEffect(() => {
+        fetchPosts(topicId)
+    }, [fetchPosts])
+
+    posts = posts.filter(post => (
+        parseInt(post.attributes.topic_id) === parseInt(topicId)
+    ))
 
     return (
         <div>
             <h1>Topic - {topic && topic.attributes.title}</h1>
+            {posts.map(post => (
+                <p key={post.id}>{post.attributes.text}</p>
+            ))}
+
         </div>
     )
 }
@@ -15,10 +33,12 @@ const TopicContainer = ({ match, subsections, topics }) => {
 const mapStateToProps = state => ({
     sections: state.sections,
     subsections: state.subsections,
-    topics: state.topics
+    topics: state.topics,
+    posts: state.posts,
 })
 
 const mapDispatchToProps = dispatch => ({
+    fetchPosts: topicId => dispatch(fetchPosts(topicId)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(TopicContainer)
