b9c07961d6886cb266c3227fdbe5284fd9ca61c7
jim <jbmbroomfield@gmail.com>
2021-11-06 16:51:13 +1100

    Clean up

--- src/components/PostContent.js
+++ src/components/PostContent.js
@@ -4,17 +4,6 @@ import { bbCodeObjects } from '../textEditor/bbCode/BBCodeObject'
 import bbCodeParsing from '../textEditor/bbCode/BBCodeParsing.js'
 
 const PostContent = ({ text, tag, createdAt }) => {
-    // createdAt = new Date(createdAt)
-    // const dateOptions = {
-    //     weekday: 'long',
-    //     year: 'numeric',
-    //     month: 'long',
-    //     day: 'numeric',
-    //     hour: 'numeric',
-    //     minute: 'numeric',
-    //     second: 'numeric',
-    // }
-    // const dateString = createdAt.toLocaleDateString("en-US", dateOptions)
 
     const renderPostHeader = () => (
         <>
