40f9f5be50fc9c96cc637ba4a07e32f27f6a6e74
jim <jbmbroomfield@gmail.com>
2021-11-10 16:55:05 +1100

    Added default avatar

--- src/components/PostInfo.js
+++ src/components/PostInfo.js
@@ -2,10 +2,13 @@ import React from 'react'
 import UserLink from './UserLink'
 
 const PostInfo = ({ user, avatarUrl }) => {
+
+    const avatarSrc = avatarUrl || require('../default_avatar.png')
+
     return (
         <div className="post-info">
             <div><UserLink user={user} /></div>
-            <div><img className="avatar" src={avatarUrl} alt="avatar" /></div>
+            <div><img className="avatar" src={avatarSrc} alt="avatar" /></div>
         </div>
     )
 }
