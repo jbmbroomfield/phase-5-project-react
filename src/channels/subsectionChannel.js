dd36a08edb1fd281fea4b86b9bf1165a0094084d
jim <jbmbroomfield@gmail.com>
2021-11-11 11:47:47 +1100

    Added subsection_update to subsectionChannel

--- src/channels/subsectionChannel.js
+++ src/channels/subsectionChannel.js
@@ -1,6 +1,6 @@
 import createSocket from './createSocket'
 
-const subsectionChannel = (subsectionId, fetchTopic) => {
+const subsectionChannel = (subsectionId, fetchTopic, fetchSubsection) => {
 
     const params = {
         channel: 'SubsectionChannel',
@@ -10,7 +10,10 @@ const subsectionChannel = (subsectionId, fetchTopic) => {
     const messageFunctions = {
         topic_update: message => {
             fetchTopic(message.topic_id)
-        }
+        },
+        subsection_update: message => {
+            fetchSubsection(message.subsection_id)
+        },
     }
 
     return createSocket(params, messageFunctions)
