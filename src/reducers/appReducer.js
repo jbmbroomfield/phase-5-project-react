a5a90404e45e500be75151de63c018e084324640
jim <jbmbroomfield@gmail.com>
2021-11-18 20:37:27 +1100

    Reset Redux on user logout

---
+++ src/reducers/appReducer.js
@@ -0,0 +1,31 @@
+import { combineReducers } from "redux"
+
+import currentUserReducer from "./currentUserReducer"
+import usersReducer from "./usersReducer"
+import sectionsReducer from './sectionsReducer'
+import subsectionsReducer from "./subsectionsReducer"
+import topicsReducer from './topicsReducer'
+import postsReducer from "./postsReducer"
+import userTopicsReducer from "./userTopicsReducer"
+import notificationsReducer from "./notificationsReducer"
+import bottomPopUpReducer from "./bottomPopUpReducer"
+import draftsReducer from "./draftsReducer"
+import topicDisplaysReducer from "./topicDisplaysReducer"
+import scrollIdReducer from "./scrollIdReducer"
+
+const appReducer = combineReducers({
+    currentUser: currentUserReducer,
+    users: usersReducer,
+    sections: sectionsReducer,
+    subsections: subsectionsReducer,
+    topics: topicsReducer,
+    posts: postsReducer,
+    userTopics: userTopicsReducer,
+    notifications: notificationsReducer,
+    bottomPopUp: bottomPopUpReducer,
+    drafts: draftsReducer,
+    topicDisplays: topicDisplaysReducer,
+    scrollId: scrollIdReducer,
+})
+
+export default appReducer
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,31 +1,13 @@
-import { combineReducers } from "redux"
+import appReducer from "./appReducer";
 
-import currentUserReducer from "./currentUserReducer"
-import usersReducer from "./usersReducer"
-import sectionsReducer from './sectionsReducer'
-import subsectionsReducer from "./subsectionsReducer"
-import topicsReducer from './topicsReducer'
-import postsReducer from "./postsReducer"
-import userTopicsReducer from "./userTopicsReducer"
-import notificationsReducer from "./notificationsReducer"
-import bottomPopUpReducer from "./bottomPopUpReducer"
-import draftsReducer from "./draftsReducer"
-import topicDisplaysReducer from "./topicDisplaysReducer"
-import scrollIdReducer from "./scrollIdReducer"
+const rootReducer = (state, action) => {
 
-const rootReducer = combineReducers({
-    currentUser: currentUserReducer,
-    users: usersReducer,
-    sections: sectionsReducer,
-    subsections: subsectionsReducer,
-    topics: topicsReducer,
-    posts: postsReducer,
-    userTopics: userTopicsReducer,
-    notifications: notificationsReducer,
-    bottomPopUp: bottomPopUpReducer,
-    drafts: draftsReducer,
-    topicDisplays: topicDisplaysReducer,
-    scrollId: scrollIdReducer,
-})
+    if (action.type === 'REMOVE_CURRENT_USER') {
+        return appReducer(undefined, action)
+    }
+
+    return appReducer(state, action)
+
+}
 
 export default rootReducer
