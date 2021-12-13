e961d00228f6b51596e3bd293d3b10b522ecae05
jim <jbmbroomfield@gmail.com>
2021-12-13 17:54:24 +1100

    Quick subsections load

--- src/react/main/components/SubsectionSummary.js
+++ src/react/main/components/SubsectionSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Spinner from 'react/sharedComponents/Spinner'
 import LastPost from './LastPost'
 
 const SubsectionSummary = ({
@@ -10,7 +11,9 @@ const SubsectionSummary = ({
     }
 
     const attributes = subsection.attributes
-    const lastPost = attributes.last_post
+    const topicCount = attributes.topic_count || <Spinner />
+    const postCount = attributes.post_count || <Spinner />
+    const lastPost = attributes.last_post ? <div><LastPost lastPost={attributes.last_post} showTopic={true} /></div> : <Spinner />
 
     return (
         <div className="subsection-summary">
@@ -19,15 +22,9 @@ const SubsectionSummary = ({
                     {subsection.attributes.title}
                 </Link>
             </div>
-            <div>
-                {attributes.topic_count}
-            </div>
-            <div>
-                {attributes.post_count}
-            </div>
-            <div>
-                <div><LastPost lastPost={lastPost} showTopic={true} /></div>
-            </div>
+            <div>{topicCount}</div>
+            <div>{postCount}</div>
+            <div>{lastPost}</div>
         </div>
     )
 }
--- src/redux/actions/subsectionsActions.js
+++ src/redux/actions/subsectionsActions.js
@@ -9,7 +9,14 @@ export const fetchSubsections = () => (
     dispatch => {
         API.get('subsections')
         .then(json => {
-            json.data && dispatch(addSubsections(json.data))
+            const subsections = json.data
+            if (!subsections) {
+                return
+            }
+            dispatch(addSubsections(json.data))
+            for (const subsection of subsections) {
+                dispatch(fetchSubsection(subsection.attributes.slug))
+            }
         })
     }
 )
